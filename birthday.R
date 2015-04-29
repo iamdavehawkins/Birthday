462c67247a1b1662dad66471373ae11289af84f5
iamdavehawkins <iamdavehawkins@gmail.com>
2015-04-29 11:52:38 -0400

    saves images, sets theme

--- birthday.R
+++ birthday.R
@@ -6,15 +6,17 @@
 library(ggplot2)
 library(reshape2)
 library(scales)
-setwd('~/documents/workspace/birthday/')
 
 # Read in csv [Count, Year, Year, ...]
-bd <- read.csv("birthday.csv")
+bd <- read.csv("./birthday.csv")
 
 # Melt data so Year becomes variable
 mbd <- melt(bd, id = "Count")
 
+theme_set(theme_bw(15))
+
 #Cumulative Plot
+png('lines.png')
 ggplot(mbd, aes(x = strptime(value, format = "%H:%M"), 
 						y = Count, 
 						color = variable))+
@@ -22,24 +24,26 @@ ggplot(mbd, aes(x = strptime(value, format = "%H:%M"),
 		ylab("# of wall posts")+
 		xlab("Time")+
 		ggtitle("Facebook Birthday Wall Posts by Time of Day")+
-		theme_bw()+
 		scale_x_datetime(labels = date_format("%I:%M %p"),
 				breaks = date_breaks("4 hours"))+
 		theme(legend.position = c(.15, .85),
 				legend.title = element_blank())+
 		scale_color_manual(values = c("Blue","Green","Orange","Purple","Red"))+
 		ylim(0,100)
+dev.off()
 
 # Make Histogram
 mbd$hour <- as.character(format(strptime(mbd$value, format = "%H:%M"), "%H"))
 freq <- aggregate(Count ~ variable + hour, data = mbd, FUN=length)
 
+png("histogram.png")
 ggplot(freq, aes(x=hour, y=Count, fill=variable, color=variable))+
 		geom_bar(stat="identity", position="dodge")+
-		facet_grid(variable~.)+
-		theme_bw()
+		facet_grid(variable~.)
+dev.off()
 
 # Plot data as points and a 3rd-order polynomial smoother
+png("curvefit.png")
 ggplot(mbd, aes(x = strptime(value, format = "%H:%M"), 
 						y = Count, 
 						color = variable))+
@@ -49,15 +53,16 @@ ggplot(mbd, aes(x = strptime(value, format = "%H:%M"),
 		ylab("# of wall posts")+
 		xlab("Time")+
 		ggtitle("Facebook Birthday Wall Posts by Time of Day")+
-		theme_bw()+
 		scale_x_datetime(labels = date_format("%I:%M %p"),
 				breaks = date_breaks("4 hours"))+
 		theme(legend.position = c(.15, .85),
 				legend.title = element_blank())+
 		scale_color_manual(values = c("Blue","Green","Orange","Purple","Red"))+
 		ylim(0,100)
+dev.off()
 
-# Multiple windows 
+# Multiple windows
+png("facets.png")
 ggplot(mbd, aes(x = strptime(value, format = "%H:%M"), 
 						y = Count))+
 		geom_point(size = 1)+
@@ -66,10 +71,10 @@ ggplot(mbd, aes(x = strptime(value, format = "%H:%M"),
 		ylab("# of wall posts")+
 		xlab("Time")+
 		ggtitle("Birthday Wall Posts by Time of Day\n3rd-order polyfit")+
-		theme_bw()+
 		scale_x_datetime(labels = date_format("%I:%M %p"),
 				breaks = date_breaks("15 hours"))+
 		theme(legend.position = c(.15, .85),
 				legend.title = element_blank())+
 		facet_grid(.~variable)
+dev.off()
 
